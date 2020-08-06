ba77cdc5749df9e0abff98922b72972586324276
Tolnut <55943564+jagadeesh-tolnut@users.noreply.github.com>
2020-08-06 11:37:59 +0530

    Delete search_job.py

--- JT/search_job.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-a=input('Enter a job title : ')
-wiki_link="https://en.wikipedia.org/wiki/"
-wiki_job_link = wiki_link + a
-webbrowser.open(wiki_job_link)
-
-# https://en.wikipedia.org/wiki/Plumber
-
